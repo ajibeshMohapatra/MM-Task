2c46c63170f179658eec3710bc03b349e6de4d61
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2020-04-10 22:17:45 +0530

    Web Task-2 & App Task - 2

--- Web Task/ciblog/application/views/posts/view.php
+++ Web Task/ciblog/application/views/posts/view.php
@@ -3,3 +3,8 @@
 <div class="post-body">
 	<?php echo $post['body']; ?>
 </div>
+<hr>
+<a class="btn btn-default pull-left" href="edit/<?php echo $post['slug']; ?>">Edit</a>
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+<input type="submit" value="Delete" class="btn btn-danger ">
+</form>
--- Web Task/ciblog/application/views/templates/header.php
+++ Web Task/ciblog/application/views/templates/header.php
@@ -4,6 +4,7 @@
 	<link rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/style.css">
+      <script src="http://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
 </head>
 <body>
 	<nav class="navbar navbar-inverse">
@@ -16,6 +17,9 @@
 					<li><a href="<?php echo base_url(); ?>">Home</a></li>
 					<li><a href="<?php echo base_url(); ?>about">About</a></li>
 					<li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
 				</ul>
 			</div>	
 		</div>	
