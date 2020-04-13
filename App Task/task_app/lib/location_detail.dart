12a6c90ee06ffe17e70b122ce3c420c420d74aa8
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2020-04-13 23:46:26 +0530

    App Task - 4

--- App Task/task_app/lib/location_detail.dart
+++ App Task/task_app/lib/location_detail.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
+import 'models/location.dart';
 
 class LocationDetail extends StatelessWidget{
+  final Location location;
+
+  LocationDetail(this.location);
+
 @override
 Widget build(BuildContext context){
  return Scaffold(
       appBar: AppBar(
-      title: Text("Hello World !"),
+      title: Text(location.name),
       ),
   body: Column(
   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
   crossAxisAlignment: CrossAxisAlignment.stretch,
-  children: [
-  section("one.", Colors.red),
-    section("two.", Colors.blue),
-    section("three.", Colors.green),
-  ],
+  children: _renderFacts(location),
   ),
   );
 }
-Widget section(String title,Color color){
-  return Container(
-    decoration: BoxDecoration(
-      color: color,
-    ),
-    child: Text(title),
-  );
+List<Widget> _renderFacts(Location location){
+  var result = List<Widget>();
+  for (int i=0; i < location.facts.length; i++){
+    result.add(_sectionTitle(location.facts[1].title));
+    result.add(_sectionText(location.facts[1].text));
+  }
+  return result;
+}
+
+Widget _sectionTitle(String text){
+  return Text(text);
+}
+Widget _sectionText(String text){
+  return Text(text);
 }
 }
 
