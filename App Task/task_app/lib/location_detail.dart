0ea8a5b722491c2d9138c00117bc5b1086de38ba
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2020-04-13 23:41:40 +0530

    Revert "App Task - 4"
    This reverts commit 02f4a3b143ae741c389d9f5bc27273c98ddbce0a.

--- App Task/task_app/lib/location_detail.dart
+++ App Task/task_app/lib/location_detail.dart
@@ -1,38 +1,30 @@
 import 'package:flutter/material.dart';
-import 'models/location.dart';
 
 class LocationDetail extends StatelessWidget{
-  final Location location;
-
-  LocationDetail(this.location);
-
 @override
 Widget build(BuildContext context){
  return Scaffold(
       appBar: AppBar(
-      title: Text(location.name),
+      title: Text("Hello World !"),
       ),
   body: Column(
   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
   crossAxisAlignment: CrossAxisAlignment.stretch,
-  children: _renderFacts(location),
+  children: [
+  section("one.", Colors.red),
+    section("two.", Colors.blue),
+    section("three.", Colors.green),
+  ],
   ),
   );
 }
-List<Widget> _renderFacts(Location location){
-  var result = List<Widget>();
-  for (int i=0; i < location.facts.length; i++){
-    result.add(_sectionTitle(location.facts[1].title));
-    result.add(_sectionText(location.facts[1].text));
-  }
-  return result;
-}
-
-Widget _sectionTitle(String text){
-  return Text(text);
-}
-Widget _sectionText(String text){
-  return Text(text);
+Widget section(String title,Color color){
+  return Container(
+    decoration: BoxDecoration(
+      color: color,
+    ),
+    child: Text(title),
+  );
 }
 }
 
