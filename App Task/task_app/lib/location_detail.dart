b60d1bf997a696c89405797411c7f983e1175a70
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2020-04-15 18:49:06 +0530

    App Task - 6

--- App Task/task_app/lib/location_detail.dart
+++ App Task/task_app/lib/location_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/location.dart';
+import 'styles.dart';
 
 class LocationDetail extends StatelessWidget{
   final Location location;
@@ -10,7 +11,7 @@ class LocationDetail extends StatelessWidget{
 Widget build(BuildContext context){
  return Scaffold(
       appBar: AppBar(
-      title: Text(location.name),
+      title: Text(location.name , style: Styles.navBarTitle,),
       ),
   body: Column(
   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -41,15 +42,12 @@ Widget _sectionTitle(String text){
     padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
       child: Text(text,
         textAlign: TextAlign.left,
-          style: TextStyle(
-      fontSize: 25.0,
-      color: Colors.black,
-    )));
+          style: Styles.headerLarge));
 }
 Widget _sectionText(String text){
   return Container(
       padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
-       child: Text(text));
+       child: Text(text, style: Styles.textDefault));
 }
   Widget _bannerImage(String url, double height){
     return Container (
